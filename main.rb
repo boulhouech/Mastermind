1e878f4439386b4c7628d61ca4304a2282f6e68c
boulhouech <slim.boulahouech@gmail.com>
2024-05-25 12:01:18 +0100

    Initiate Game class

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ class Board
   attr_accessor :game_colors, :rows_of_game, :code_pegs
 
   def initialize
-    @game_colors = ["red", "blue", "green", "yellow", "white", "black"]
+    @game_colors = ["%red", "blue", "green", "yellow", "white", "black"]
     @rows_of_game = Array.new(10) { Array.new(4) }
     @code_pegs = []
   end
@@ -14,5 +14,20 @@ class Board
   end
 end
 
-board = Board.new
-board.display_board
+class Game
+  def initialize(code_maker, code_braker)
+    @code_maker = code_maker
+    @code_braker = code_braker
+
+    puts "\nWelcome to the Mastermind game!"
+    puts "Type 'P' to play against a person."
+    puts "Type 'C' to play against the computer.\n"
+  end
+end
+
+
+
+
+
+# board = Board.new
+# board.display_board
