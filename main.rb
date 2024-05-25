d68ede358eea7d6369dc69e0f03fe9236f8bb64c
boulhouech <slim.boulahouech@gmail.com>
2024-05-25 15:49:18 +0100

    adding the first logic of play_round method

--- main.rb
+++ main.rb
@@ -15,18 +15,64 @@ class Board
 end
 
 class Game
-  def initialize(code_maker, code_braker)
+  def initialize
     @code_maker = code_maker
     @code_braker = code_braker
+    @players = []
+
+
+  end
+  #while free_rows false
+
+  def play_round
+    attr_reader :opponent
 
     puts "\nWelcome to the Mastermind game!"
     puts "Type 'P' to play against a person."
     puts "Type 'C' to play against the computer.\n"
+
+    opponent = gets.chomp.upcase
+
+    until @players.include?("C" || "P")
+      if opponent == "C"
+        play_against_computer
+      end
+      if opponent == "P"
+        play_agaisnt_human
+      else
+        "ERROR, CHOSE AGAAIN"
+      end
+    end
+    end
+
+
+  end
+
+  def
+
+  end
+
+
+
+
+
+
+
+
+  #def current_player(player)
+
+  #end
+
+  #end
+
+
+
   end
 end
 
 
-
+game = Game.new
+game.play_round
 
 
 # board = Board.new
