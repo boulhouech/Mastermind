fe45c71f5e6643ec47df49f331915ca9a6a2e53f
boulhouech <slim.boulahouech@gmail.com>
2024-05-26 22:52:15 +0100

    adding code maker method

--- main.rb
+++ main.rb
@@ -32,9 +32,9 @@ class Game
     opponent = gets.chomp.upcase
     cheak_opponent(opponent)
 
-    def cheak_opponent(player)
-      until opponent == "C" || opponent == "P"
-        case opponent
+    def cheak_opponent(op)
+      until op == "C" || opponent == "P"
+        case op
         when "C"
           play_against_computer
         when "P"
@@ -44,43 +44,48 @@ class Game
         end
       end
     end
+  end
 
-    def play_against_human
-      puts "Enter Your Name PLayer One"
-      player_one = gets.chomp.downcase
-      @players << player_one
-    end
+  def play_against_human
+    puts 'Enter Your Name PLayer One'
+    player_one = gets.chomp.downcase
+    @players << player_one
 
-    end
+    puts 'Enter Your Name Player Two'
+    player_two = gets.chomp.downcase
+    @players << player_two
 
+    @players[0] = @code_maker
+    @players[1] = @code_braker
 
+  puts "Game Started #{@players[0]} Is The Code Maker\n"
+  code_maker(@players[0])
 
-
-
-
-
+  puts "Game Started #{@players[1]} Is The Code Braker\n"
+  code_braker(@players[1])
 
   end
 
+  def code_maker(player)
+    code = @code_pegs
+    getting_colrs = ""
+    MAX_LENGTH == 4
 
+    until code.length == MAX_LENGTH
+      puts "#{player} choose from this colots #{@game_colors}"
+      getting_colrs = STDIN.noecho(&:gets).chomp
+      code << getting_colrs
+    end
+  end
 
+  def code_braker
 
-
-
-
-
-
-
-
-  #def current_player(player)
-
-  #end
-
-  #end
-
+  end
 
 end
-end
+
+
+
 
 
 
