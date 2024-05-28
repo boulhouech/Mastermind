8917514fcdc04f79d1b8f88d28bd4e532c2ccb0b
boulhouech <slim.boulahouech@gmail.com>
2024-05-28 23:35:30 +0100

    adding hard_mode method

--- computer_play.rb
+++ computer_play.rb
@@ -76,4 +76,32 @@ class Computer
       puts "Computer attempts: #{computer_attempts}"
     end
   end
+
+  def play_hard_mode
+    @board = Board.new
+    track_of_guessing = []
+    code_to_break = @board.code_pegs.dup
+    computer_attempts = 0
+
+    until computer_attempts >= MAX_ATTEMPTS
+      computer_attempts += 1
+      computer_guess = generate_guess_hard(track_of_guessing, code_to_break)
+      track_of_guessing << computer_guess
+
+      puts "Computer's guess: #{computer_guess}"
+      puts "White peg means correct color in the wrong place."
+      puts "Red peg means correct color in the correct place."
+      puts "Enter feedback in the format: [number of red pegs] [number of white pegs]."
+      puts "For example, '2 1' means 2 colors are correct and in the right place, and 1 color is correct but in the wrong place."
+
+      user_feedback = gets.chomp
+      red_pegs, white_pegs = user_feedback.split.map(&:to_i)
+
+      break if red_pegs == code_to_break.length
+
+      puts "Computer attempts: #{computer_attempts}"
+    end
+  end
+
+  private
 end
