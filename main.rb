ff845964a1e2697218e23fa77f4d4761c54edb46
boulhouech <slim.boulahouech@gmail.com>
2024-05-28 16:04:26 +0100

    re-thinking the the way computer should  break the code

--- main.rb
+++ main.rb
@@ -179,62 +179,7 @@ class Game
     @board.code_pegs = computer_code.dup
   end
 
-  def computer_break_code
-    computer_attempt  = @board.game_colors.sample(4)
-    result = @board.code_pegs
-
-
-
-    until computer_attempt.eql?(result) || computer_attempt >= MAX_ATTEMPTS
-      if not_identical?(computer_attempt, result)
-        next_move()
-      end
-
-      def next_move(old_guess)
-        
-        @previous_guesses = [] if @previous_guesses.nil?
-        @previous_guesses << old_guess
-
-        # Generate a new guess that doesn't include any of the previous elements
-        new_guess = []
-        while new_guess.length < 4
-          color = @board.game_colors.sample
-          new_guess << color unless @previous_guesses.any? { |guess| guess.include?(color) }
-        end
-
-        new_guess
-      end
-    end
-  end
-
-  def check_right_position(result_array, computer_attempt_array)
-    right_position = []
-    computer_attempt_array.each_with_index do |color, index|
-      if color == result_array[index]
-        right_position << index
-      end
-    right_position
-  end
-
-  def check_misplaced(result_array, computer_attempt_array)
-    correct_but_misplaced = []
-
-    computer_attempt_array.each_with_index do |color, index|
-
-      result_array.include?(color)
-        correct_but_misplaced << computer_attempt.index(color)
-      end
-    end
-  end
-
-  def not_identical?(computer_attempt_array, result_array)
-    if (computer_attempt_array - result_array).empty? && (result_array - computer_attempt_array).empty?
-  end
-
-
-
-
-end
+  
 
 game = Game.new
 game.play_round
